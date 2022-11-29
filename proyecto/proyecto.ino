1d0dd9fa1cc8124ff10b2a7b6ad2fe8f7f96cd9c
Oriana Acosta Joaquim <o.acosta.2019@alumnos.urjc.es>
2022-11-29 10:55:58 +0100

    Declaran variables
    Signed-off-by: Oriana Acosta Joaquim <o.acosta.2019@alumnos.urjc.es>

---
+++ proyecto/proyecto.ino
@@ -0,0 +1,180 @@
+/*
+* Copyright (C) 2022 by Roberto Calvo-Palomino
+*
+*
+*  This programa is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with RTL-Spec.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Authors: Roberto Calvo-Palomino <roberto.calvo [at] urjc [dot] es>
+*/
+
+// Subject: Sistemas Empotrados y de Tiempo Real
+// Universidad Rey Juan Carlos, Spain
+
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+// Variables
+#define TRIG_PIN 13  
+#define ECHO_PIN 12 
+#define MAX_DISTANCE 200 
+#define PIN_ITR20001_LEFT   A2
+#define PIN_ITR20001_MIDDLE A1
+#define PIN_ITR20001_RIGHT  A0
+// Enable/Disable motor control.
+//  HIGH: motor control enabled
+//  LOW: motor control disabled
+#define PIN_Motor_STBY 3
+
+// Group A Motors (Right Side)
+// PIN_Motor_AIN_1: Digital output. HIGH: Forward, LOW: Backward
+#define PIN_Motor_AIN_1 7
+// PIN_Motor_PWMA: Analog output [0-255]. It provides speed.
+#define PIN_Motor_PWMA 5
+
+// Group B Motors (Left Side)
+// PIN_Motor_BIN_1: Digital output. HIGH: Forward, LOW: Backward
+#define PIN_Motor_BIN_1 8
+// PIN_Motor_PWMB: Analog output [0-255]. It provides speed.
+#define PIN_Motor_PWMB 6
+
+#define team_name "Robotitos"
+#define ID_EQUIPO  "9"
+#define server "193.147.53.2"
+#define port 21883
+
+const char* ssid = "sensoresurjc";
+const char* password = "Goox0sie_WZCGGh25680000";
+
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, server, port, team_name, team_name, ID_EQUIPO);
+Adafruit_MQTT_Subscribe program = Adafruit_MQTT_Subscribe(&mqtt, team_name "/feeds/program");
+void MQTT_connect();
+
+void setup(){
+  Serial.begin(9600);
+
+  // line-tracking
+  pinMode(PIN_ITR20001_LEFT, INPUT);
+  pinMode(PIN_ITR20001_MIDDLE, INPUT);
+  pinMode(PIN_ITR20001_RIGHT, INPUT);
+
+  // ultrasonic
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  
+  // Setup MQTT subscription for program feed.
+  mqtt.subscribe(&program);
+}
+ 
+void loop(){
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // this is our 'wait for incoming subscription packets' busy subloop
+  // try to spend your time here
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    // Check if its the onoff button feed
+    Serial.print(F("On-Off button: "));
+    
+    if (strcmp((char *)program.lastread, "ON") == 0) {
+      Serial.print(F("low "));
+    }
+    if (strcmp((char *)program.lastread, "OFF") == 0) {
+      Serial.print(F("high "));
+    }
+  }
+
+  // ping the server to keep the mqtt connection alive
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+}
+
+float line_L(void) {
+  return analogRead(PIN_ITR20001_LEFT);
+}
+float line_M(void) {
+  return analogRead(PIN_ITR20001_MIDDLE);
+}
+float line_R(void) {
+  return analogRead(PIN_ITR20001_RIGHT);
+}
+
+float UltraSonic_DistanceCm()
+{
+  float distance = 0;
+  digitalWrite(TRIG_PIN, LOW);
+  float time = millis();
+  while (millis() - time < 2);
+  digitalWrite(TRIG_PIN, HIGH);
+  while (millis() - time < 12);
+  digitalWrite(TRIG_PIN, LOW);
+  distance = pulseIn(ECHO_PIN, HIGH) / 58;
+  return distance;
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
