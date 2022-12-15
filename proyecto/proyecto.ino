9801260b97a99f5a35afd92ef7585dea44b30c89
Oriana Acosta Joaquim <o.acosta.2019@alumnos.urjc.es>
2022-12-15 09:41:30 +0100

    hola
    Signed-off-by: Oriana Acosta Joaquim <o.acosta.2019@alumnos.urjc.es>

--- proyecto/proyecto.ino
+++ proyecto/proyecto.ino
@@ -20,17 +20,6 @@
 
 // Subject: Sistemas Empotrados y de Tiempo Real
 // Universidad Rey Juan Carlos, Spain
-/*
-#ifdef ESP32
-  #include <WiFi.h>
-#else
-  #include <ESP8266WiFi.h>
-#endif
-
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"*/
-#include <Arduino_FreeRTOS.h>
-
 #include "Arduino.h"
 
 // Variables
@@ -38,9 +27,9 @@
 #define ECHO_PIN 12 
 #define MAX_DISTANCE 200 
 // INFRA RED SENSOR:
-#define PIN_ITR20001_LEFT   'A2'
-#define PIN_ITR20001_MIDDLE 'A1'
-#define PIN_ITR20001_RIGHT  'A0'
+#define PIN_ITR20001xxxL A2
+#define PIN_ITR20001xxxM A1
+#define PIN_ITR20001xxxR A0
 
 
 // Enable/Disable motor control.
@@ -70,21 +59,11 @@
 const char* ssid = "sensoresurjc";
 const char* password = "Goox0sie_WZCGGh25680000";
 
-
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-
-//WiFiClient client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-
-// Adafruit_MQTT_Client mqtt(&client, server, port, team_name, team_name, ID_EQUIPO);
-//Adafruit_MQTT_Subscribe program = Adafruit_MQTT_Subscribe(&mqtt, team_name "/feeds/program");
-
-//void MQTT_connect();
-// Messages
 int state, previus_state;
 long tracking_time = 0;
 bool line_lost_searching;
+int speed_A = 100;
+int speed_B = 100;
 String start[][2] = {{"team_name: ", team_name}, {"id: ", ID_EQUIPO}, {"action: ", "START_LAP"}};
 String finish[][2] = {{"team_name: ", team_name}, {"id: ", ID_EQUIPO}, {"action: ", "END_LAP"}, {"time: ", (char *)tracking_time}};
 String obstacle[][2] = {{"team_name: ", team_name}, {"id: ", ID_EQUIPO}, {"action: ", "OBSTACLE_DETECTED"}};
@@ -99,76 +78,83 @@ void setup(){
   Serial.begin(9600);
 
   // line-tracking
-  pinMode(PIN_ITR20001_LEFT, INPUT);
-  pinMode(PIN_ITR20001_MIDDLE, INPUT);
-  pinMode(PIN_ITR20001_RIGHT, INPUT);
+  pinMode(PIN_ITR20001xxxL, INPUT);
+  pinMode(PIN_ITR20001xxxM, INPUT);
+  pinMode(PIN_ITR20001xxxR, INPUT);
+
+  pinMode(PIN_Motor_PWMA, OUTPUT);
+  pinMode(PIN_Motor_PWMB, OUTPUT);
+  pinMode(PIN_Motor_AIN_1, OUTPUT);
+  pinMode(PIN_Motor_BIN_1, OUTPUT);
 
   // ultrasonic
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIG_PIN, OUTPUT);
 
-  // Connect to WiFi access point.
-  Serial.println(); Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
   //Inicio de la máquina de estados:
   state = 0;
-
-  /*WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-
-  Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
-  
-  // Setup MQTT subscription for program feed.
-  mqtt.subscribe(&program);*/
 }
 
 
 void forward() {
-  analogWrite(PIN_Motor_PWMA, SPEED);
-  analogWrite(PIN_Motor_PWMB, SPEED);
-  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  /*digitalWrite(PIN_Motor_STBY, true);
+  digitalWrite(PIN_Motor_AIN_1, LOW);
+  analogWrite(PIN_Motor_PWMA, speed_A);
   digitalWrite(PIN_Motor_BIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMB, speed_B);*/
   Serial.println("go forward!");
 }
 
 void left(){
-  analogWrite(PIN_Motor_PWMA, SPEED);
-  analogWrite(PIN_Motor_PWMB, SPEED);
-  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  /*digitalWrite(PIN_Motor_STBY, true);
+  digitalWrite(PIN_Motor_AIN_1, LOW);
+  analogWrite(PIN_Motor_PWMA, speed_A);
   digitalWrite(PIN_Motor_BIN_1, LOW);
+  analogWrite(PIN_Motor_PWMB, speed_B);
+  */
   Serial.println("go left!");
 }
 
 void right(){
-  analogWrite(PIN_Motor_PWMA, SPEED);
-  analogWrite(PIN_Motor_PWMB, SPEED);
-  digitalWrite(PIN_Motor_AIN_1, LOW);
+  /*digitalWrite(PIN_Motor_STBY, true);
+  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMA, speed_A);
   digitalWrite(PIN_Motor_BIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMB, speed_B);*/
   Serial.println("go right!");
 }
 
 void stop(){
-  analogWrite(PIN_Motor_PWMA, SPEED);
-  analogWrite(PIN_Motor_PWMB, SPEED);
-  Serial.println("stop!")
+  /*digitalWrite(PIN_Motor_STBY, false);
+  analogWrite(PIN_Motor_PWMB, 0);
+  analogWrite(PIN_Motor_PWMA, 0); */
+  Serial.println("stop!");
 
   
 }
- 
+
 void loop(){
-  bool stop = stop_car;
+  bool stop = stop_car();
   if (stop) {
     state = 3;
   } else {
     state = line_tracking();
   }
+  Serial.println(state);
+  if (state == 0) {
+    forward();
+  } else {
+    if (state == 1) {
+      if(line_R() > 100) {
+        right();
+      } else {
+        if(line_L() > 100) {
+          left();
+        } 
+      }
+    }
+  } //else if state == 2 {
+
   // estado 0 avanzar
   // estado 1 girar
   // estado 2 encontrar linea
@@ -182,11 +168,10 @@ int line_tracking() {
   float lineM = line_M();
   float lineR = line_R();
   int estado;
-
-  if (lineM < 800) {
+  if (lineM > 100) {
     estado = 0;
   } else {
-    if ((lineR < 800) || (lineL < 800)) {
+    if ((lineR > 100) || (lineL > 100)) {
       estado = 1;
     } else {
       estado = 2;
@@ -197,87 +182,48 @@ int line_tracking() {
 }
 
 float line_L(void) {
-  return analogRead(PIN_ITR20001_LEFT);
+  return analogRead(PIN_ITR20001xxxL);
 }
 float line_M(void) {
-  return analogRead(PIN_ITR20001_MIDDLE);
+  return analogRead(PIN_ITR20001xxxM);
 }
 float line_R(void) {
-  return analogRead(PIN_ITR20001_RIGHT);
+  return analogRead(PIN_ITR20001xxxR);
 }
 
 float UltraSonic_DistanceCm()
 {
-  float distance = 0;
   digitalWrite(TRIG_PIN, LOW);
   float time = millis();
   while (millis() - time < 2);
   digitalWrite(TRIG_PIN, HIGH);
   while (millis() - time < 12);
   digitalWrite(TRIG_PIN, LOW);
-  distance = pulseIn(ECHO_PIN, HIGH) / 58;
+  float speedOfSound = 0.03313 + 0.0000606;
+
+  unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration / 2.0 * speedOfSound;
   return distance;
-}
- 
-/* void loop(){
-  // Ensure the connection to the MQTT server is alive (this will make the first
-  // connection and automatically reconnect when disconnected).  See the MQTT_connect
-  // function definition further below.
-  //MQTT_connect();
-
-  // this is our 'wait for incoming subscription packets' busy subloop
-  // try to spend your time here
-
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(5000))) {
-    // Check if its the onoff button feed
-    Serial.print(F("On-Off button: "));
-    
-    if (strcmp((char *)program.lastread, "ON") == 0) {
-      Serial.print(F("low "));
-    }
-    if (strcmp((char *)program.lastread, "OFF") == 0) {
-      Serial.print(F("high "));
-    }
-  }
-
-  // ping the server to keep the mqtt connection alive
-  if(! mqtt.ping()) {
-    mqtt.disconnect();
-  }
-}*/
-  estado = 0;
-}
- 
-void loop(){
-  bool stop = stop_car;
-  if (stop) {
-    estado = 3;
-  }
-  if(line_M){
-    forward();
-  }
-  else if(line_R) {
-    right();
-    while(line_R);
-  }
-  else if(line_L) {
-    left();
-    while(line_L);
-  } 
-  // estado 0 avanzar
-  // estado 1 girar
-  // estado 2 encontrar linea
-  // estado 3 parar
 }
 
 bool stop_car() {
   float distance = UltraSonic_DistanceCm();
-  if (distance < 10){
-    for (int i = 0;i <3; i++) {
+  if (distance < 8){ //80
+    for (int i = 0;i < 3; i++) {
       Serial.write("data");
     }
     return true;
   }
   return false;
+}
+
+void serialPort_read() {
+  static String SerialPortData = "";
+  uint8_t c = "";
+  if (Serial.available() > 0) {
+    while (c != '}' && Serial.available() > 0) {
+      c = Serial.read();
+      SerialPortData += (char)c;
+    }
+  }
 }
